5a5bd32901cefbcd7a64e12d3ff240a49b30e659
Shivam-apna <sksingh@aapnainfotech.com>
2023-01-02 13:27:08 +0530

    added chat component.

---
+++ application/backend/src/main/java/io/typerefinery/websight/models/components/Chart.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2022 Typerefinery.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//namespace
+package io.typerefinery.websight.models.components;
+import static org.apache.sling.models.annotations.DefaultInjectionStrategy.OPTIONAL;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import org.apache.sling.models.annotations.Default;
+import lombok.Getter;
+@Model(adaptables = Resource.class, defaultInjectionStrategy = OPTIONAL)
+public class Chart {
+  // private String label;
+
+  // @Getter
+  // @Inject
+  // @Default (values = "300")
+  // public String data;
+
+  // @Getter
+  // @Inject
+  // @Default (values = "#42A5F5")
+  // public String backgroundColor;
+
+  // @Getter
+  // @Inject
+  // @Default (values = "#64B5F6")
+  // public String hoverBackgroundColor;
+
+}
