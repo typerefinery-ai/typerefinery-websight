f43acbbcbab03260c98ac9a2acbb9aec1145ea86
HariKarthykAAPNA <hkarthikkeyyan@aapnainfotech.com>
2023-01-05 13:09:46 +0530

    minor code changes in navbar and map error fixed.

--- application/backend/src/main/resources/apps/typerefinery/components/content/map/script.js
+++ application/backend/src/main/resources/apps/typerefinery/components/content/map/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function (e) {
-  console.log("helll")
-  const key = "WTh8FTa6cT027rlVBBbG";
-  const map = new maplibregl.Map({
-    container: "map", // container's id or the HTML element in which MapLibre GL JS will render the map
-    style: `https://api.maptiler.com/maps/hybrid/style.json?key=${key}`, // style URL
-    center: [16.62662018, 49.2125578], // starting position [lng, lat]
-    zoom: 0, // starting zoom
-  });
-  map.addControl(new maplibregl.NavigationControl(), "top-right");
-  map.on("error", function (err) {
-    throw new Error("WTh8FTa6cT027rlVBBbG");
-  });
+  if (document.getElementById("map")) {
+    const key = "WTh8FTa6cT027rlVBBbG";
+    const map = new maplibregl.Map({
+      container: "map", // container's id or the HTML element in which MapLibre GL JS will render the map
+      style: `https://api.maptiler.com/maps/hybrid/style.json?key=${key}`, // style URL
+      center: [16.62662018, 49.2125578], // starting position [lng, lat]
+      zoom: 0, // starting zoom
+    });
+    map.addControl(new maplibregl.NavigationControl(), "top-right");
+    map.on("error", function (err) {
+      console.log("MAP ERROR", err)
+    });
+  }
+
 });
