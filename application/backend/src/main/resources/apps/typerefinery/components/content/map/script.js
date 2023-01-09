fd552fd3edc1f0874457d5159b8ea60a4b4e81ea
Shivam-apna <sksingh@aapnainfotech.com>
2023-01-09 18:34:18 +0530

    added leaflet map component.

--- application/backend/src/main/resources/apps/typerefinery/components/content/map/script.js
+++ application/backend/src/main/resources/apps/typerefinery/components/content/map/script.js
@@ -1,55 +1,29 @@
 $(document).ready(function (e) {
   if (document.getElementById("map")) {
     const key = "WTh8FTa6cT027rlVBBbG";
-    const dataId = "dcb0900e-992a-4ff5-9819-861b4df293e9";
-    const dataId1 = "b70a0972-99a0-4539-98eb-35e04f0c052d";
-    const map = new maplibregl.Map({
-      container: "map", // container's id or the HTML element in which MapLibre GL JS will render the map
-      style: `https://api.maptiler.com/maps/bright-v2/style.json?key=${key}`, // style URL
-      center: [9.145, 40.4897], // starting position [lng, lat]
-      zoom: 1.5, // starting zoom
-    });
-    map.addControl(new maplibregl.NavigationControl(), "top-right");
-    fetch(`https://api.maptiler.com/data/${dataId}/features.json?key=${key}`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (geojson) {
-        map.on("load", function () {
-          map.addSource("geojson-overlay", {
-            type: "geojson",
-            data: geojson,
-          });
-          map.addLayer({
-            id: "20cada51-ac22-461a-b60d-ee895337314d",
-            type: "fill",
-            source: "geojson-overlay",
-            layout: {},
-            paint: {
-              "fill-color": "#98b",
-              "fill-opacity": 0.3,
-            },
-          });
-        });
-      });
-    map.on("load", function () {
-      map.addSource("gps_tracks", {
-        type: "geojson",
-        data: `https://api.maptiler.com/data/${dataId1}/features.json?key=${key}`,
-      });
-      map.addLayer({
-        id: "gps_tracks",
-        type: "fill",
-        source: "gps_tracks",
-        layout: {},
-        paint: {
-          "fill-color": "#f4a261",
-          "fill-opacity": 0.3,
-        },
-      });
-    });
-    map.on("error", function (err) {
-      console.log("MAP ERROR", err);
-    });
+    var map = L.map('map').setView([9.145, 40.4897], 2);
+
+    //To Add geoJSON
+    
+    L.geoJSON(polyJSON,{
+      style:{
+        fillColor:'#e76f51',
+        fillOpacity:0.5,
+        stroke:false
+      }
+    }).addTo(map)
+    L.geoJSON(polyJSON1,{
+      style:{
+        fillColor:'#98b',
+        fillOpacity:0.5,
+        stroke:false
+      }
+    }).addTo(map)
+    
+    //add map style
+        L.tileLayer('https://api.maptiler.com/maps/bright-v2/256/{z}/{x}/{y}.png?key=WTh8FTa6cT027rlVBBbG', {          
+        maxZoom: 19,
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }).addTo(map); 
   }
 });
