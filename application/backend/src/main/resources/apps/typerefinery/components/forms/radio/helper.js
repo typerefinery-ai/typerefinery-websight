a8fc4a623284a1f3de5b4bb92dc1d2090416333f
HariKarthykAAPNA <hkarthikkeyyan@aapnainfotech.com>
2023-01-19 18:28:48 +0530

    removed console log.

--- application/backend/src/main/resources/apps/typerefinery/components/forms/radio/helper.js
+++ application/backend/src/main/resources/apps/typerefinery/components/forms/radio/helper.js
@@ -1,7 +1,6 @@
 window.Typerefinery.Components.Forms.RadioButton = {
     getData: function() {
         let data = {};
-        console.log("document.querySelectorAll", document.querySelectorAll('[data-module="vue-radioButton"]'))
         document.querySelectorAll('[data-module="vue-radioButton"]').forEach(_ => {
             let modelName = _.getAttribute("name");
             modelName.trim();
