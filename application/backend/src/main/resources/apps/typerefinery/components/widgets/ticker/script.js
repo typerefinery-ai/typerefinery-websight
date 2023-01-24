75e3a84dbfdcd005edb2a893f22987f06e45336c
HariKarthykAAPNA <hkarthikkeyyan@aapnainfotech.com>
2023-01-24 12:20:51 +0530

    code refactored (removed unused function, updated comments).

--- application/backend/src/main/resources/apps/typerefinery/components/widgets/ticker/script.js
+++ application/backend/src/main/resources/apps/typerefinery/components/widgets/ticker/script.js
@@ -1,22 +1,6 @@
-function getDataFromDataSource(defaultData, path, id) {
-  const fetchAndUpdateView = async () => {
-    try {
-      const response = await fetch(path).then((res) => res.json());
-
-      !response.value
-        ? updateTickerComponent(defaultData, id)
-        : updateTickerComponent(response, id);
-    } catch (error) {
-      updateTickerComponent(defaultData, id);
-    }
-  };
-  fetchAndUpdateView();
-}
-
+// Update the UI
 function updateTickerComponent(tickerData, component) {
-  // const component = document.getElementById(id);
-  // if (component.getAttribute("data-topic") === id) {
-  const tickerHtmlWithJsonValue = `
+  const tickerHtml = `
             <div class="body">
                  <div class="title">${tickerData.title}</div>
                  <div class="content">
@@ -34,49 +18,48 @@ function updateTickerComponent(tickerData, component) {
                 </div>
             </div>
             <div class="icon ${tickerData.icon}"></div>
-    `;
+  `;
 
-  component.innerHTML = tickerHtmlWithJsonValue;
-  // }
+  component.innerHTML = tickerHtml;
 }
 
-//Update ticker using tms connection and its payload
+// Update ticker using tms connection and its payload
 function tickerComponentConnectedViaTMS(topic, host) {
   setTimeout(() => {
     connectTMS(topic, host);
-  }, 5000);
+  }, 2500);
 }
+
+
 // TMS connection
 function connectTMS(topic, host) {
-  var component = document.getElementById(topic);
-  console.log("id", document.getElementById(topic));
+  const component = document.getElementById(topic);
   // listen for messages
   window.addEventListener(
     window.MessageService.Client.events.MESSAGE,
     function (message) {
-      console.log(
-        `ui event: ${window.MessageService.Client.events.MESSAGE}`,
-        message.detail,
-        "hello"
-      );
-      const payload = message.detail.data.payload;
-      console.log("pay", payload);
-      const parsedPayload = JSON.parse(payload);
-      const resultPayload = parsedPayload.data;
-      // document.getElementById("messages")?.appendChild(messageItem);
-      updateTickerComponent(resultPayload, component);
+      const messageData = message?.detail?.data?.payload;
+      if (messageData) {
+        const payload = JSON.parse(messageData);
+        const { data } = payload;
+        if (data) {
+          updateTickerComponent(data, component);
+        }
+      }
     }
   );
   function payload_insert(data) {
     console.log("payload_insert", data);
   }
+
   // connect to websocket
   window.MessageService.Client.connect(host, function () {
     console.log("tms connected cms.");
     window.MessageService.Client.subscribe("payload_insert", payload_insert);
   });
 }
-// Update ticker using datasource JSON
+
+// Update ticker using dataSource JSON
 function tickerComponentConnectedViaJSON(jsonUrl, component) {
   const fetchAndUpdateView = async () => {
     try {
@@ -91,36 +74,41 @@ function tickerComponentConnectedViaJSON(jsonUrl, component) {
   };
   fetchAndUpdateView();
 }
+
 // Default data
 function tickerComponentConnectedViaInitialData(component) {
-  var model = component.getAttribute("data-model");
+  const model = component.getAttribute("data-model");
   const parsedModel = JSON.parse(model);
-  var defaultData = {
+
+  const defaultData = {
     title: parsedModel.title,
     value: parsedModel.value,
     icon: parsedModel.icon,
     indicatorType: parsedModel.indicatorType,
     indicatorValue: parsedModel.indicatorValue,
   };
-  // Rendering the template
 
   updateTickerComponent(defaultData, component);
 }
 
-// ticker component
+// Initial Function
 $(document).ready(function (e) {
   Array.from(document.querySelectorAll("#ticker")).forEach((component) => {
-    var componentTopic = component.getAttribute("data-topic");
-    var componentHost = component.getAttribute("data-host");
-    var componentDataSource = component.getAttribute("data-source");
+    const componentTopic = component.getAttribute("data-topic");
+    const componentHost = component.getAttribute("data-host");
+    const componentDataSource = component.getAttribute("data-source");
+
+    // Data can be updated via TMS Connection.
     if (componentTopic && componentHost) {
       component.setAttribute("id", componentTopic);
       tickerComponentConnectedViaTMS(componentTopic, componentHost, component);
     }
-    //datasourcs json
+    // Data can be updated via Data Source JSON
     else if (componentDataSource) {
       tickerComponentConnectedViaJSON(componentDataSource, component);
-    } else {
+    }
+    // Data can be updated via the default values from the model.
+    else {
       tickerComponentConnectedViaInitialData(component);
     }
   });
