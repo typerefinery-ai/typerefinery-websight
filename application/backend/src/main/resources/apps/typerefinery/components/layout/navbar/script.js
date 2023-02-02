64f1f98c2a8244ec83e8a59f7fbc2ad8f5b6c012
HariKarthykAAPNA <hkarthikkeyyan@aapnainfotech.com>
2023-02-02 17:48:20 +0530

    updated switchTheme functions.

--- application/backend/src/main/resources/apps/typerefinery/components/layout/navbar/script.js
+++ application/backend/src/main/resources/apps/typerefinery/components/layout/navbar/script.js
@@ -1,10 +1,10 @@
 async function switchTheme() {
     const themeStyles = document.getElementById("themeStyles");
     if(themeStyles) {
-        const currentTheme = style.getAttribute('active') || 'dark';
+        const currentTheme = themeStyles.getAttribute('active') || 'dark';
         const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-        style.setAttribute("active", newTheme)
-        style.setAttribute("href", `/apps/typerefinery/web_root/${newTheme}.css`)
+        themeStyles.setAttribute("active", newTheme)
+        themeStyles.setAttribute("href", `/apps/typerefinery/web_root/${newTheme}.css`)
         await localStorage.setItem("theme", newTheme);
         setTimeout(() => {
             Object.entries(window.Typerefinery.Components.Graphs.Items).forEach(_ => {
