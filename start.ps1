8af31e3976504ce76ce1fa1ca9259c7ffed6f909
wildone <max.barrass@gmail.com>
2023-02-05 21:39:13 +1100

    change path after running containers.

--- start.ps1
+++ start.ps1
@@ -1,1 +1,7 @@
-cd .\environment\local\ && docker compose up
+$CURRENT_PATH = Get-Location
+Set-Location -Path ".\environment\local\"
+try {
+    docker compose up
+} finally {
+    Set-Location -Path $CURRENT_PATH
+}
