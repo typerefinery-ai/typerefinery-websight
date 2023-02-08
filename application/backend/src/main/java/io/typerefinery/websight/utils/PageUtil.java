dce7362cb96174efea3f8933d45b1d782924be32
wildone <max.barrass@gmail.com>
2023-02-08 13:24:03 +1100

    update copyright.

--- application/backend/src/main/java/io/typerefinery/websight/utils/PageUtil.java
+++ application/backend/src/main/java/io/typerefinery/websight/utils/PageUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 TypeRefinery.io
+ * Copyright (C) 2023 Typerefinery.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
