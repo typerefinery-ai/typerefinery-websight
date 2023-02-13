c21af24e01c6087ea915f64c542765c501373105
HariKarthykAAPNA <hkarthikkeyyan@aapnainfotech.com>
2023-02-13 19:21:04 +0530

    Form component updated.

---
+++ application/backend/src/main/resources/apps/typerefinery/components/forms/form/clientlibs/behaviour.js
@@ -0,0 +1,13 @@
+window.Typerefinery = window.Typerefinery || {};
+window.Typerefinery.Components = Typerefinery.Components || {};
+window.Typerefinery.Components.Forms = Typerefinery.Components.Forms || {};
+window.Typerefinery.Components.Forms.Form = Typerefinery.Components.Forms.Form || {};
+
+(function (ns, document) {
+    "use strict";
+    $(document).ready(function () {
+        $("#form").each(function () {
+            ns.addEventListener(this);
+        });
+    });
+})(window.Typerefinery.Components.Forms.Form, document);
