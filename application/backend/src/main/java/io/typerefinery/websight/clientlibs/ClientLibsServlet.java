62e36af3ee33f74b3ca656a170ac639b4f86dfc8
wildone <max.barrass@gmail.com>
2023-02-21 05:15:28 +1100

    add new line after each file.

--- application/backend/src/main/java/io/typerefinery/websight/clientlibs/ClientLibsServlet.java
+++ application/backend/src/main/java/io/typerefinery/websight/clientlibs/ClientLibsServlet.java
@@ -389,6 +389,8 @@ public class ClientLibsServlet extends SlingSafeMethodsServlet  {
             if (!ResourceUtil.isNonExistingResource(resource)) {
                 InputStream inputStream = resource.adaptTo(InputStream.class);
                 out.write(IOUtils.toString(inputStream, "UTF-8"));
+                //add new line after each file
+                out.append(System.lineSeparator());
             }
             return true;
         } catch (IOException e) {
