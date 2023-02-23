ab8c31a443318638c05c5eb4b4af28af09faec6a
wildone <max.barrass@gmail.com>
2023-02-23 15:57:35 +1100

    add error handling.

--- application/backend/src/main/java/io/typerefinery/websight/models/components/BaseFormComponent.java
+++ application/backend/src/main/java/io/typerefinery/websight/models/components/BaseFormComponent.java
@@ -53,15 +53,17 @@ public class BaseFormComponent extends BaseComponent {
     @PostConstruct
     protected void init() {
         // check if selectors are present
-        String selectors = request.getRequestPathInfo().getSelectorString();
-        if (StringUtils.isNotBlank(selectors)) {
-            String[] selectorArray = StringUtils.split(selectors, ".");
-            if (selectorArray.length >= 2) {
-                // check if id is present and update component id
-                if (ArrayUtils.contains(selectorArray, "id")) {
-                    String value = selectorArray[ArrayUtils.indexOf(selectorArray, "id")+1];
-                    if (StringUtils.isNotBlank(value)) {
-                        this.id = value;
+        if (request != null && request.getRequestPathInfo() != null) {
+            String selectors = request.getRequestPathInfo().getSelectorString();
+            if (StringUtils.isNotBlank(selectors)) {
+                String[] selectorArray = StringUtils.split(selectors, ".");
+                if (selectorArray.length >= 2) {
+                    // check if id is present and update component id
+                    if (ArrayUtils.contains(selectorArray, "id")) {
+                        String value = selectorArray[ArrayUtils.indexOf(selectorArray, "id")+1];
+                        if (StringUtils.isNotBlank(value)) {
+                            this.id = value;
+                        }
                     }
                 }
             }
