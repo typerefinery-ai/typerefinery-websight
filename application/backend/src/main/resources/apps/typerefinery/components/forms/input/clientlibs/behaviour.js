5c7bbb0a3e329c85d9006167a0246b96d2be3756
Harikarthyk <hari.jsmith494@gmail.com>
2023-03-01 18:26:22 +0530

    document.querySelectorAll added for iterating input fields.

--- application/backend/src/main/resources/apps/typerefinery/components/forms/input/clientlibs/behaviour.js
+++ application/backend/src/main/resources/apps/typerefinery/components/forms/input/clientlibs/behaviour.js
@@ -6,10 +6,9 @@ window.Typerefinery.Components.Forms.Input = Typerefinery.Components.Forms.Input
 (function (ns, document) {
     "use strict";
     $(document).ready(function () {
-        $("#field").each(function () {
-            const input = this.getElementsByTagName("input");
-            if(input.length > 0)
-                ns.init(input[0]);
-        });
+        document.querySelectorAll("#field").forEach(($fieldComponent) => {
+            const $input = $fieldComponent.querySelector("input");
+            ns.init($input);
+        })
     });
 })(window.Typerefinery.Components.Forms.Input, document);
