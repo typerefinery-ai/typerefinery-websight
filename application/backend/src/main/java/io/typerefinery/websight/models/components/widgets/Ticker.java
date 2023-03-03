536e9a940c7454fd363ae61edf92b2beac120b79
wildone <max.barrass@gmail.com>
2023-03-03 22:42:00 +1100

    update defaults for ticker.

--- application/backend/src/main/java/io/typerefinery/websight/models/components/widgets/Ticker.java
+++ application/backend/src/main/java/io/typerefinery/websight/models/components/widgets/Ticker.java
@@ -3,6 +3,8 @@ package io.typerefinery.websight.models.components.widgets;
 import static org.apache.sling.models.annotations.DefaultInjectionStrategy.OPTIONAL;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
+
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
@@ -10,6 +12,7 @@ import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.ExporterOption;
 import org.apache.sling.models.annotations.Model;
+import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Component;
 import io.typerefinery.websight.models.components.BaseComponent;
 import io.typerefinery.websight.services.flow.FlowService;
@@ -32,87 +35,122 @@ public class Ticker extends BaseComponent implements FlowComponent {
     private static final String DEFAULT_ID = "ticker";
     private static final String DEFAULT_MODULE = "tickerComponent";
 
+    private static final String PROPERTY_TITLE = "title";
+    private static final String DEFAULT_TITLE = "Sample Card";
+
+    private static final String PROPERTY_VALUE = "value";
+    private static final String DEFAULT_VALUE = "12.5k";
+
+    private static final String PROPERTY_ICON = "icon";
+    private static final String DEFAULT_ICON = "pi pi-database";
+
+    private static final String PROPERTY_BADGE = "badge";
+    private static final String DEFAULT_BADGE = "";
+
+    private static final String PROPERTY_INDICATOR_TYPE = "indicatorType";
+    private static final String DEFAULT_INDICATOR_TYPE = "pi pi-arrow-up";
+
+    private static final String PROPERTY_INDICATOR_VALUE = "indicatorValue";
+    private static final String DEFAULT_INDICATOR_VALUE = "12.k";
+
+    private static final String PROPERTY_INDICATOR_VALUE_PRECISION = "indicatorValuePrecision";
+    private static final String DEFAULT_INDICATOR_VALUE_PRECISION = "";
+
+    private static final String PROPERTY_DATASOURCE = "dataSource";
+    private static final String DEFAULT_DATASOURCE = "";
+
+    private static final String PROPERTY_WEBSOCKET_HOST = "websocketHost";
+    private static final String DEFAULT_WEBSOCKET_HOST = "ws://localhost:8112/$tms";
+
+    private static final String PROPERTY_WEBSOCKET_TOPIC = "websocketTopic";
+    private static final String DEFAULT_WEBSOCKET_TOPIC = "";
+
+    private static final String PROPERTY_FLOWAPI_TOPIC = "flowapi_topic";
+    private static final String DEFAULT_FLOWAPI_TOPIC = "";
+
+    private static final String PROPERTY_VARIANT = "variant";
+    private static final String DEFAULT_VARIANT = "primaryTicker";
+
+    private static final String PROPERTY_BACKGROUND_CLASS = "backGroundClass";
+    private static final String DEFAULT_BACKGROUND_CLASS = "";    
+
+
     @Getter
     @Inject
-    @Default(values = "Sample Card")
+    @Named(PROPERTY_TITLE)
+    @Nullable
     public String title;
 
     @Getter
     @Inject
-    @Default(values = "12.5k")
+    @Named(PROPERTY_VALUE)
+    @Nullable
     public String value;
 
     @Getter
     @Inject
-    @Default(values = "pi pi-database")
+    @Named(PROPERTY_ICON)
+    @Nullable
     public String icon;
 
     @Getter
     @Inject
+    @Named(PROPERTY_BADGE)
+    @Nullable
     public String badge;
 
     @Getter
     @Inject
-    @Default(values = "pi pi-arrow-up")
+    @Named(PROPERTY_INDICATOR_TYPE)
+    @Nullable
     public String indicatorType;
 
     @Getter
     @Inject
-    @Default(values = "12.k")
+    @Named(PROPERTY_INDICATOR_VALUE)
+    @Nullable
     public String indicatorValue;
     
     @Getter
     @Inject
-    @Default(values = "")
+    @Named(PROPERTY_INDICATOR_VALUE_PRECISION)
+    @Nullable
     public String indicatorValuePrecision;
 
     @Getter
     @Inject
+    @Named(PROPERTY_DATASOURCE)
+    @Nullable
     public String dataSource;
 
     @Getter
     @Inject
-    @Default(values = "ws://localhost:8112/$tms")
+    @Named(PROPERTY_WEBSOCKET_HOST)
+    @Nullable
     public String websocketHost;
 
     @Getter
     @Inject
+    @Named(PROPERTY_WEBSOCKET_TOPIC)
+    @Nullable
     public String websocketTopic;
 
     @Getter
     @Inject
-    @Default(values = "")
+    @Named(PROPERTY_FLOWAPI_TOPIC)
+    @Nullable
     public String flowapi_topic;
 
-    @Inject
-    @Getter
-    @Default(values = "")
-    private String sizeType;
-
-    @Inject
-    @Getter
-    @Default(values = "")
-    private String sizeValue;
-
-    @Inject
-    @Getter
-    @Default(values = "")
-    private String alignmentHorizontal;
-
-    @Inject
-    @Getter
-    @Default(values = "")
-    private String alignmentVirtical;
-
     @Getter
     @Inject
-    @Default(values = "primaryTicker")
+    @Named(PROPERTY_VARIANT)
+    @Nullable
     public String variant;
 
-
     @Inject
     @Getter
-    // @Default (values = "")
+    @Named(PROPERTY_BACKGROUND_CLASS)
+    @Nullable
     public String backGroundClass;
 
     @Override
@@ -135,6 +173,46 @@ public class Ticker extends BaseComponent implements FlowComponent {
     protected void init() {
         this.module = DEFAULT_MODULE;
         super.init();
+        if (StringUtils.isBlank(title)) {
+            this.title = DEFAULT_TITLE;
+        }
+        if (StringUtils.isBlank(value)) {
+            this.value = DEFAULT_VALUE;
+        }
+        if (StringUtils.isBlank(icon)) {
+            this.icon = DEFAULT_ICON;
+        }
+        if (StringUtils.isBlank(badge)) {
+            this.badge = DEFAULT_BADGE;
+        }
+        if (StringUtils.isBlank(indicatorType)) {
+            this.indicatorType = DEFAULT_INDICATOR_TYPE;
+        }
+        if (StringUtils.isBlank(indicatorValue)) {
+            this.indicatorValue = DEFAULT_INDICATOR_VALUE;
+        }
+        if (StringUtils.isBlank(indicatorValuePrecision)) {
+            this.indicatorValuePrecision = DEFAULT_INDICATOR_VALUE_PRECISION;
+        }
+        if (StringUtils.isBlank(dataSource)) {
+            this.dataSource = DEFAULT_DATASOURCE;
+        }
+        if (StringUtils.isBlank(websocketHost)) {
+            this.websocketHost = DEFAULT_WEBSOCKET_HOST;
+        }
+        if (StringUtils.isBlank(websocketTopic)) {
+            this.websocketTopic = DEFAULT_WEBSOCKET_TOPIC;
+        }
+        if (StringUtils.isBlank(flowapi_topic)) {
+            this.flowapi_topic = DEFAULT_FLOWAPI_TOPIC;
+        }
+        if (StringUtils.isBlank(variant)) {
+            this.variant = DEFAULT_VARIANT;
+        }
+        if (StringUtils.isBlank(backGroundClass)) {
+            this.backGroundClass = DEFAULT_BACKGROUND_CLASS;
+        }
+
         if (StringUtils.isBlank(this.websocketTopic)) {
             this.websocketTopic = this.flowapi_topic;
         }
