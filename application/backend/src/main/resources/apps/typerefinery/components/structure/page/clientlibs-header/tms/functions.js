1f96395da012098e339e9472bc21c6c1164a00f8
wildone <max.barrass@gmail.com>
2023-03-05 04:48:48 +1100

    refactor to use payload and topic,

--- application/backend/src/main/resources/apps/typerefinery/components/structure/page/clientlibs-header/tms/functions.js
+++ application/backend/src/main/resources/apps/typerefinery/components/structure/page/clientlibs-header/tms/functions.js
@@ -7,96 +7,93 @@ window.MessageService = window.MessageService || {};
 window.MessageService.Client = MessageService.Client || {};
 
 (function (ns, clientNs, document, window) {
-    "use strict";
+  "use strict";
 
-    ns.registery = {};
+  ns.registery = {};
 
-    ns.persistData = (key, data) => {
-        localStorage.setItem(key, data);
-    };
-    
-    
-    ns.hostAdded = (newHost) => {
-        console.log("new host", newHost)
-        const listOfHost = JSON.parse(localStorage.getItem("tmsHost") || '[]');
-        const filteredHost = listOfHost.filter(host => host === newHost);
-        if (filteredHost.length === 0) {
-            listOfHost.push(newHost);
-            localStorage.setItem("tmsHost", JSON.stringify(listOfHost));
-        }
+  ns.persistData = (key, data) => {
+    localStorage.setItem(key, data);
+  };
+
+  ns.hostAdded = (newHost) => {
+    console.log("new host", newHost);
+    const listOfHost = JSON.parse(localStorage.getItem("tmsHost") || "[]");
+    const filteredHost = listOfHost.filter((host) => host === newHost);
+    if (filteredHost.length === 0) {
+      listOfHost.push(newHost);
+      localStorage.setItem("tmsHost", JSON.stringify(listOfHost));
+    }
+  };
+
+  ns.persistMessagePayloadData = (message) => {
+    let payload = message?.detail?.data?.payload || null;
+    if (payload) {
+      payload = JSON.parse(payload);
+      if (payload.data) {
+        ns.persistData(payload.topic, JSON.stringify(payload.data));
+        return payload;
+      }
+    }
+    return null;
+  };
+
+  ns.registerToTms = (host, topic, key, callbackFn) => {
+    ns.hostAdded(host);
+    ns.registery[host] = ns.registery[host] || {};
+    ns.registery[host][topic] = ns.registery[host][topic] || {};
+    ns.registery[host][topic][key] = callbackFn;
+  };
+
+  ns.connect = () => {
+    const listOfHost = JSON.parse(localStorage.getItem("tmsHost") || "[]");
+
+    function payload_insert(data) {
+      console.log(
+        "--------------------------Payload Inserted ------------------------"
+      );
     }
 
-    ns.persistMessagePayloadData = (message) => {
-        let payload = message?.detail?.data?.payload || null;
-        if (payload) {
-            payload = JSON.parse(payload);
-            if (payload.data) {
-                ns.persistData(payload.topic, JSON.stringify(payload.data));
-                return payload;
-            }
-        }
-        return null;
-    };
-
-    ns.registerToTms = (host, topic, key, callbackFn) => {
-        ns.hostAdded(host);
-        ns.registery[host] = ns.registery[host] || {};
-        ns.registery[host][topic] = ns.registery[host][topic] || {};
-        ns.registery[host][topic][key] = callbackFn;
-    };
-
-    ns.connect = () => {
-        const listOfHost = JSON.parse(localStorage.getItem("tmsHost") || '[]');
-
-        function payload_insert(data) {
-            console.log("--------------------------Payload Inserted ------------------------");
-        }
-
-        listOfHost.forEach(host => {
-            try {
-                // connect to websocket
-                clientNs?.connect(host, function () {
-                    clientNs?.subscribe("payload_insert", payload_insert);
-                });
-            } catch (error) {
-                console.error(error);
-            }
-
+    listOfHost.forEach((host) => {
+      try {
+        // connect to websocket
+        clientNs?.connect(host, function () {
+          clientNs?.subscribe("payload_insert", payload_insert);
         });
+      } catch (error) {
+        console.error(error);
+      }
+    });
 
+    // listen to messages.
+    window.addEventListener(clientNs?.events.MESSAGE, function (message) {
+      console.log(["tms message", message]);
+      let messagePayload = message?.detail?.data?.payload || null;
+      let messageTopic = message?.detail?.data?.topic || null;
+      if (messagePayload && messageTopic) {
+        console.log(["tms message payload", messageTopic, messagePayload]);
+        ns.persistData(messageTopic, messagePayload);
+        console.log(["tms message save to local storage", messageTopic]);
+        // TODO: get host from the message.
+        const host = Object.entries(ns.registery)[0];
+        console.log(["tms message host", host]);
+        // first idx[key, values].
+        if (host) {
+          // grab the topic from the host.
+          const topic = host[1][messageTopic];
+          console.log(["tms message host topic", topic]);
+          if (topic) {
+            console.log(["tms message call topic callback", topic]);
+            // foreach with the topic to trigger the callback.
+            Object.values(topic).forEach((callback) =>
+              callback(JSON.parse(messagePayload))
+            );
+          }
+        }
+      }
+    });
+  };
 
-        // listen to messages.
-        window.addEventListener(
-            clientNs?.events.MESSAGE,
-            function (message) {
-                console.log("-------------------------- 12 MESSAGE RECEIVED ------------------------")
-                let payload = message?.detail?.data?.payload || null;
-                if (payload) {
-                    payload = JSON.parse(payload);
-                    if(!payload.topic || !payload.data) {
-                        return;
-                    }
-                    ns.persistData(payload.topic, JSON.stringify(payload.data));
-                    // TODO: get host from the message.
-                    const host = Object.entries(ns.registery)[0];
-                    
-                    // first idx[key, values].
-                    if (host) {
-                        // grab the topic from the host.
-                        const topic = host[1][payload.topic];
-                        if (topic) {
-                            // foreach with the topic to trigger the callback.
-                            Object.values(topic).forEach(callback => callback(payload.data));
-                        }
-                    }
-                }
-            }
-        );
-
-        
-    };
-
-    ns.init = () => {
-        ns.connect();
-    };
+  ns.init = () => {
+    ns.connect();
+  };
 })(Typerefinery.Page.Tms, MessageService.Client, document, window);
