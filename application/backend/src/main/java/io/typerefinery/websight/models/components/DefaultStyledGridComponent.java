959a5719e56a7722bdc31474f07d1ced84a37d79
wildone <max.barrass@gmail.com>
2023-03-06 19:39:32 +1100

    add missing import.

--- application/backend/src/main/java/io/typerefinery/websight/models/components/DefaultStyledGridComponent.java
+++ application/backend/src/main/java/io/typerefinery/websight/models/components/DefaultStyledGridComponent.java
@@ -26,6 +26,7 @@ import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
