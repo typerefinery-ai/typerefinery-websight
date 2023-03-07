bbdb79e05021de85b2d98fbc65fbc16099f9e2e0
Harikarthyk <harikarthyk1407@gmail.com>
2023-03-07 22:29:39 +0530

    returned null incase if child is empty.

--- application/backend/src/main/resources/apps/typerefinery/components/layout/sidebar/clientlibs/functions.js
+++ application/backend/src/main/resources/apps/typerefinery/components/layout/sidebar/clientlibs/functions.js
@@ -32,11 +32,12 @@ window.Typerefinery.Components.Layout.Sidebar = Typerefinery.Components.Layout.S
                     continue;
                 }
                 const _obj = obj[keyItr];
+                const children = fetchMenuItemHelper(_obj);
                 result.push({
                     href: _obj["jcr:content"].key,
                     text: capitalizeFirstLetter(_obj["jcr:content"].title),
                     icon: _obj["jcr:content"].icon,
-                    nodes: fetchMenuItemHelper(_obj)
+                    nodes: children.length === 0 ? null : children
                 });
             }
             return result;
@@ -55,7 +56,7 @@ window.Typerefinery.Components.Layout.Sidebar = Typerefinery.Components.Layout.S
             data: formattedMenuItems, 
             levels: 10, 
             color: "#FFF",
-            backColor: "#7386D5",
+            backColor: "#4456a5",
             onhoverColor: "#9aadff",
             borderColor: "#9aadff",
             showBorder: false,
@@ -65,8 +66,8 @@ window.Typerefinery.Components.Layout.Sidebar = Typerefinery.Components.Layout.S
             selectedBackColor: "#7386D5"
         });
 
-        $(id).treeview('enableAll', { silent: true });
-        $(id).treeview('collapseAll', { silent: true });
+        $(sidebarComponentId).treeview('enableAll', { silent: true });
+        $(sidebarComponentId).treeview('collapseAll', { silent: true });
     }
 
 })(Typerefinery.Components.Layout.Sidebar, Typerefinery.Components, document, window);
