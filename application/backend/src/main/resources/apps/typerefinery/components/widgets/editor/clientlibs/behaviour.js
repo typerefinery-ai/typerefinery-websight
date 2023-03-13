8a627d4c6791ece925831d9eade2205e099bfb87
Harikarthyk <harikarthyk1407@gmail.com>
2023-03-13 16:02:00 +0530

    editor component added.

---
+++ application/backend/src/main/resources/apps/typerefinery/components/widgets/editor/clientlibs/behaviour.js
@@ -0,0 +1,13 @@
+window.Typerefinery = window.Typerefinery || {};
+window.Typerefinery.Components = Typerefinery.Components || {};
+window.Typerefinery.Components.Widgets = Typerefinery.Components.Widgets || {};
+window.Typerefinery.Components.Widgets.Editor = Typerefinery.Components.Widgets.Editor || {};
+
+(function (ns, document) {
+    "use strict";
+    $(document).ready(function () {
+        $('[component=editor]').each(function () {
+            ns?.init(this);
+        });
+    });
+})(Typerefinery.Components.Widgets.Editor, document);
