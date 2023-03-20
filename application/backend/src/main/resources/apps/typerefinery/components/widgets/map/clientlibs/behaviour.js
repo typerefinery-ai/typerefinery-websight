826a5f32c6a2aac3195cda66cbd86347faeb226a
Rahul139-stack <rshaw@aapnainfotech.com>
2023-03-20 16:19:57 +0530

    made request changes

--- application/backend/src/main/resources/apps/typerefinery/components/widgets/map/clientlibs/behaviour.js
+++ application/backend/src/main/resources/apps/typerefinery/components/widgets/map/clientlibs/behaviour.js
@@ -3,7 +3,7 @@ window.Typerefinery.Components = Typerefinery.Components || {};
 window.Typerefinery.Components.Widgets = Typerefinery.Components.Widgets || {};
 window.Typerefinery.Components.Widgets.Map = Typerefinery.Components.Widgets.Map || {};
 window.Typerefinery.Components.Widgets.Map.LeafletMap = Typerefinery.Components.Widgets.Map.LeafletMap || {};
-console.log("behaviour.js")
+
 ;(function (ns, document) {
     "use strict";
     $(document).ready(function () {
