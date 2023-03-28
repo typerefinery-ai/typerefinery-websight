bd5e840b33fa60efa9638b41485c4940a61349bc
HariKarthykAAPNA <harikarthyk1407@gmail.com>
2023-03-28 11:19:20 +0530

    feat: utils functions are added to componentNs.

--- application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/components.js
+++ application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/components.js
@@ -13,4 +13,24 @@ window.Typerefinery.VueData = Typerefinery.VueData || {};
     ns.getComponentConfig = ($component) => {
       return $($component).data('model') || {};
     };
+    ns.replaceRegex = (str, obj) => {
+        return str.replace(/{{(\w+)}}/gm, function(match, key) {
+            return obj[key];
+        });
+    };
+    ns.queryToObject = (query) => {
+        const result = {};
+        query.split("&").forEach((param) => {
+            const [key, value] = param.split("=");
+            result[key] = value;
+        });
+        return result;
+    };
+    ns.objectToQuery = (obj) => {
+        return Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
+    };
+    ns.getQueryParams = () => {
+        const query = window.location.search.substring(1);
+        return ns.queryToObject(query);
+    }
 })(window.Typerefinery.Components, window.Typerefinery.VueData, document, window);
