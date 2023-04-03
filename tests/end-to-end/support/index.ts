35012206765f5dc2cf25c427c69cd4269ff429b8
Shivam-apna <sksingh@aapnainfotech.com>
2023-04-03 14:51:10 +0530

    updated dependencies version, updated commands for custom queries.

--- tests/end-to-end/support/index.ts
+++ tests/end-to-end/support/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Typerefinery.io
+ * Copyright (C) 2022 Dynamic Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,15 +23,31 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
+      getPageIframe(): Chainable<any>;
       getByTestId(testId: string): Chainable<JQuery<HTMLElement>>;
       findByTestId(testId: string): Chainable<JQuery<HTMLElement>>;
+      dragByTestId(
+        targetContextTestId: string,
+        targetTestId: string,
+        options?: { forceDrop: boolean }
+      ): void;
+      listByTestIdPrefix(testIdPrefix: string): Chainable<JQuery<HTMLElement>>;
+      saveDataAttrAsNumber(attrName: string, alias: string): void;
+      shouldAttrAsNumber(
+        attrName: string,
+        chainer: string,
+        alias: string
+      ): void;
       login(): void;
       percySnapshotWithAuth(name: string, options?: SnapshotOptions): void;
       percySnapshotPreview(name: string, options?: SnapshotOptions): void;
       percySnapshotPageEditor(name: string, options?: SnapshotOptions): void;
       percySnapshotDialog(name?: string, options?: SnapshotOptions): void;
+      checkGridProperties(): void;
     }
+
   }
+  
 }
 
 // ignore resizeObserver error occuring only in cypress
