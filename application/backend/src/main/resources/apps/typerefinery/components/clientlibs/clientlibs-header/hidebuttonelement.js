be9b33227594e57c31228fd748fb89edfbc0f44f
Rohit gupta <rogupta@aapnainfotech.com>
2023-04-12 15:13:14 +0530

    update hide property initially set collapse

--- application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/hidebuttonelement.js
+++ application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/hidebuttonelement.js
@@ -1,16 +1,21 @@
 window.Typerefinery = window.Typerefinery || {};
 window.Typerefinery.HideButtonElement = Typerefinery.HideButtonElement || {};
 
-; (function (ns, document, window) {
-    
-    ns.init = ($component, componentConfig) => {
+(function (ns, document, window) {
+  ns.init = ($component, componentConfig) => {
+    // Updating the component with Bootstrap Attributes.
+    $component.setAttribute("data-bs-toggle", "collapse");
+    $component.setAttribute(
+      "data-bs-target",
+      `${componentConfig.hideButtonElementTarget}`
+    );
 
-        // Updating the component with Bootstrap Attributes.
-        $component.setAttribute("data-bs-toggle", "collapse");
-        $component.setAttribute("data-bs-target", `${componentConfig.hideButtonElementTarget}`);
-
-        //Set collapse clase to target component which need to hhide.
-        const targetComponent= document.getElementById(`${componentConfig.hideButtonElementTarget}`)
-        targetComponent.classList.add("collapse")
-    };
+    //Set class "collapse" to hide the elements intially, which need to be toggle.
+    const targetElement = document.querySelectorAll(
+      `${componentConfig.hideButtonElementTarget}`
+    );
+    targetElement.forEach((element) => {
+      element.classList.add("collapse");
+    });
+  };
 })(Typerefinery.HideButtonElement, document, window);
