f4e42980d0b4b3aa76a8e2fea12050624d6ecde4
wildone <max.barrass@gmail.com>
2023-06-02 13:18:40 +1000

    add helper.

---
+++ release.ps1
@@ -0,0 +1,16 @@
+Param(
+  [string]$VERSION = "2023.3.31"
+)
+
+git checkout master
+git pull
+
+# delete old tag
+git tag -d v${VERSION}
+git push --delete origin v${VERSION}
+
+# create new tag
+npm version ${VERSION}
+git commit -s -a -m "chore: release v${VERSION}"
+git tag -s -a v${VERSION} -m "v${VERSION}4"
+git push && git push --tags
