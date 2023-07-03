93eae494d2662319f874270bf0cb89b6553b3f46
wildone <max.barrass@gmail.com>
2023-07-03 14:51:08 +1000

    add theme building helpers.

--- application/frontend/src/main.ts
+++ application/frontend/src/main.ts
@@ -1,2 +1,3 @@
 // Stylesheets
 import "./main.scss";
+
---
+++ application/frontend/webpack.dev.js
@@ -0,0 +1,55 @@
+const {merge} = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common.js');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = merge(common, {
+    mode: 'development',
+    plugins: [
+    //     new FileManagerPlugin({
+    //         events: {
+    //             onEnd: {
+    //                 copy: [
+    //                     {
+    //                         source: path.join(__dirname, 'src/index.html'),
+    //                         destination: path.join(__dirname, 'src/main/resources/index.html')
+    //                     }
+    //                 ]
+    //             }
+    //         }
+    //     })
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname,'src/html','index.html') ,
+            filename:'index.html'
+        })
+    ],
+    devServer: {
+        static: [
+            {
+                directory: path.resolve(__dirname, 'src/static'),
+                publicPath: '/',
+                serveIndex: true,
+            },
+            {
+                directory: path.resolve(__dirname, 'assets'),
+                publicPath: '/assets',
+                serveIndex: true,
+            }
+        ],
+        port: 8888,
+        hot: true,
+        liveReload:true,
+        // devMiddleware: {
+        //     // index: true,
+        //     // mimeTypes: { phtml: 'text/html' },
+        //     // publicPath: '/dist',
+        //     // serverSideRender: true,
+        //     writeToDisk: true,
+        // },
+        watchFiles: ['src/static/*.html', 'src/static/**/*', 'src/resources/**/*'],
+      }
+});
