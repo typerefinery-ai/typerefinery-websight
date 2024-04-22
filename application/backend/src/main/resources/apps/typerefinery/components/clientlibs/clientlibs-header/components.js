8f41e88ed92203d515c97957afb8a9968565c4a9
wildone <max.barrass@gmail.com>
2024-04-23 01:35:15 +1000

    add helpers to watch dom elements.

--- application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/components.js
+++ application/backend/src/main/resources/apps/typerefinery/components/clientlibs/clientlibs-header/components.js
@@ -108,5 +108,77 @@ window.Typerefinery.VueData = Typerefinery.VueData || {};
             });
         }, 1000);
     };
+
+    //find all selectors and run callbackFn
+    ns.initComponentBySelector = (selector, callbackFn) => {
+        if ((selector == null || selector == undefined) || (callbackFn == null || callbackFn == undefined)) {
+          console.error("initComponent: selector and callbackFn is required");
+          return;
+        }
+        //init component on all found instances
+        var elements = document.querySelectorAll(selector);
+        for (var i = 0; i < elements.length; i++) {
+          callbackFn($(elements[i]));
+        }
+    }
+
+    //observe DOM for new instances of a selector and run callbackFn
+    ns.observeDOMForSelector = (selector, callbackFn) => {
+
+      if ((selector == null || selector == undefined) || (callbackFn == null || callbackFn == undefined)) {
+        console.error("observeDOMForSelector: selector and callbackFn is required");
+        return;
+      }
+
+      //observe DOM for future instances of a selector
+      var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+      var body = document.querySelector("body");
+      var observer = new MutationObserver(function(mutations) {
+          mutations.forEach(function(mutation) {
+              var nodesArray = [].slice.call(mutation.addedNodes);
+              if (nodesArray.length > 0) {
+                  nodesArray.forEach(function(addedNode) {
+                      if (addedNode.querySelectorAll) {
+                          var elementsArray = [].slice.call(addedNode.querySelectorAll(selector));
+                          elementsArray.forEach(function(element) {
+                            console.log("new element", selector);
+                            callbackFn($(element));
+                          });
+                      }
+                  });
+              }
+          });
+      });
+
+      observer.observe(body, {
+          subtree: true,
+          childList: true,
+          characterData: true
+      });
+    }
+
+    ns.onDocumentReady = (selector, callbackFn) => {
+      if ((selector == null || selector == undefined) || (callbackFn == null || callbackFn == undefined)) {
+        console.error("onDocumentReady: selector and callbackFn is required");
+        return;
+      }
+    
+      console.group("loading searchBox");
+
+      ns.initComponentBySelector(selector, callbackFn);
+      ns.observeDOMForSelector(selector, callbackFn);
+
+      console.groupEnd();
+
+    }
+
+    ns.watchDOMForComponent = (selector, callbackFn) => {
+      if (document.readyState !== "loading") {
+          ns.onDocumentReady(selector, callbackFn);
+      } else {
+          document.addEventListener("DOMContentLoaded", ns.onDocumentReady(selector, callbackFn));
+      }
+    }
+
     ns.init();
 })(jQuery, window.Typerefinery.Components, window.Typerefinery.VueData, document, window);
