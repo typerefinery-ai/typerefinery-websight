63610c05c77ea8b1147a19429d172e656b9b5926
wildone <max.barrass@gmail.com>
2024-05-14 04:08:23 +1000

    add aocnfig field to events.

--- application/backend/src/main/java/ai/typerefinery/websight/models/dialog/EventsListItem.java
+++ application/backend/src/main/java/ai/typerefinery/websight/models/dialog/EventsListItem.java
@@ -71,6 +71,10 @@ public class EventsListItem {
     @ValueMapValue
     private String type;
 
+    @Getter
+    @ValueMapValue
+    private String config;
+
 
     @PostConstruct
     protected void init() {
