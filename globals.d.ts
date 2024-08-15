aeb8e2c46eba10a13e4cf99a11d933889e0f5427
wildone <max.barrass@gmail.com>
2024-08-16 01:50:10 +1000

    add config for intellisence.

---
+++ globals.d.ts
@@ -0,0 +1,12 @@
+//NOTE: this file is used to declare global types and interfaces to 
+// allow TW to recognize them in conmponent JS files in application/backend
+
+interface Components {
+}
+interface Typerefinery {
+    Components: Components;
+}
+
+interface Window {
+    Typerefinery: Typerefinery;
+}
